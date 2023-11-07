97dfdf953127dfc0cb3ea3cb58d91145c2605390
Dillon Teakell <teakelld@icloud.com>
2023-11-06 19:20:18 -0500

    Initial Commit

---
+++ Banking-UI/Banking_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_UIApp.swift
+//  Banking-UI
+//
+//  Created by Dillon Teakell on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Banking_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
